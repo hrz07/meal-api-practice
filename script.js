6f84799d602af6519c374505abc3301343f4549e
hrz07 <hridoy.developer@yahoo.com>
2022-02-24 02:35:29 +0600

    card created

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
-const searchBtn = ()=>{
-    fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata')
-    .then(res=>res.json())
-    .then(data=>showMeals(data.meals))
-}
+const searchBtn = () => {
+  fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=fish")
+    .then((res) => res.json())
+    .then((data) => showMeals(data.meals));
+};
 
-const showMeals = (data)=>{
-    console.log(data);
-}
+const showMeals = (data) => {
+  const contentBox = document.getElementById("content-box");
+  for (const element of data) {
+    console.log(element);
+    const div = document.createElement("div");
+    div.classList.add('mt-4')
+    div.innerHTML = `
+        <div class="card" style="width: 18rem;">
+        <img src="${element.strMealThumb}" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">${element.strMeal}</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="btn btn-primary">Details</a>
+        </div>
+      </div>
+
+        `;
+    contentBox.appendChild(div);
+  }
+};
